9b90d8cc1a92803bb814e4332cf48a172032a175
Kevin Kwon <fsw0422@gmail.com>
2014-12-23 13:22:14 -0500

    solved

--- fsw0422/VigenereAttack/decrypt.c
+++ fsw0422/VigenereAttack/decrypt.c
@@ -8,29 +8,32 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #define MAX_BUF_LEN 1024
-
-#define MIN_ASCII 0
-#define MAX_ASCII 127
 
 #define MIN_KEY_LEN 1
 #define MAX_KEY_LEN 13
 
-double square_sum_distribution(unsigned char ctxt[], size_t ctxt_len, size_t key_len);
-void guess(unsigned char ctxt[], size_t ctxt_len, size_t key_len, int offset);
-void check_space(unsigned char ctxt[], size_t ctxt_len, size_t key_len);
-void print_byte_sequence(unsigned char arr[], size_t len);
+double square_sum_distribution(unsigned char byt_arr[], size_t byt_arr_len, size_t key_len);
+void guess(unsigned char byt_arr[], size_t byt_arr_len, size_t key_len, size_t offset);
+unsigned char convert_hexstr_to_byte(char *hex_str);
+void print_byte_sequence_hex(unsigned char arr[], size_t len);
+void print_byte_sequence_char(unsigned char arr[], size_t len);
 
 int main(int argc, char *argv[])
 {
-	size_t ctxt_len;
 	unsigned char ctxt_buf[MAX_BUF_LEN];
+	size_t ctxt_len;
+	unsigned char byte_buf[MAX_BUF_LEN];
+	size_t byte_len;
 	unsigned char *key;
+	size_t key_len;
+	unsigned char dec_buf[MAX_BUF_LEN];
+	size_t dec_len;
 	FILE *fp_in;
 	size_t bytes_read;
-	size_t key_len;
-	int i;
+	int i, j;
 
 	if (argc != 2) {
 		fprintf(stderr, "usage: %s <encrypted file name>\n", argv[0]);
@@ -39,10 +42,11 @@ int main(int argc, char *argv[])
 
        	/* init */
 	memset(ctxt_buf, 0, sizeof(ctxt_buf));
-	bytes_read = 0;
-	ctxt_len = 0;
+	memset(byte_buf, 0, sizeof(byte_buf));
+	memset(dec_buf, 0, sizeof(dec_buf));
+	bytes_read = ctxt_len = byte_len = dec_len = 0;
 
-	/* read the ciphered text to buffer */
+	/* read the text to buffer */
 	fp_in = fopen(argv[1], "rb");
 	/** read the ciphered text to buffer **/
 	while (!feof(fp_in)) {
@@ -50,99 +54,116 @@ int main(int argc, char *argv[])
 		ctxt_len += bytes_read;
 	}
 	fclose(fp_in);
-	print_byte_sequence(ctxt_buf, ctxt_len);
+
+	/* convert the read text to byte array */
+	j = 0;
+	for (i = 0; i < ctxt_len; i += 2) {
+		char hex_str_byt[2];
+		unsigned char val;
+
+		hex_str_byt[0] = ctxt_buf[i];
+		hex_str_byt[1] = ctxt_buf[i + 1];
+
+		val = convert_hexstr_to_byte(hex_str_byt);
+		byte_buf[j++] = val;
+		byte_len++;
+	}
+	//print_byte_sequence_hex(byte_buf, byte_len);
 
 	/* brute-force key length (done with this)*/
 	//for (key_len = MIN_KEY_LEN; key_len <= MAX_KEY_LEN; key_len++) {
 	//	double sqr_sum;
 
-	//	sqr_sum = square_sum_distribution(ctxt_buf, ctxt_len, key_len);
-	//	printf("square sum distribution is %lf when key_len is %u\n", sqr_sum, key_len);
+	//	sqr_sum = square_sum_distribution(byte_buf, byte_len, key_len);
+	//	printf("square sum distribution is %lf when key_len is %lu\n", sqr_sum, key_len);
 	//}
 	
 	/* guess the bytes */
 	key_len = 7;
-	key = (unsigned char *) malloc(sizeof(unsigned char) * key_len);
 	/** try every offset of plain text that is <ciphertext> mod(key_len) **/
-	for (i = 0; i < key_len; i++)
-		guess(ctxt_buf, ctxt_len, key_len, i);
+	for (i = 0; i < key_len; i++) {
+		//printf("--------starting from offset %d----------\n", i);
+		guess(byte_buf, byte_len, key_len, i);
+	}
 
-	/** try to look for spaces(ASCII #32) in two contiguous blocks of ciphered text with the chunk of key_len (which is 7) **/
-	check_space(ctxt_buf, ctxt_len, key_len);	
+	/* decipher and print */
+	key = (unsigned char *) malloc(sizeof(unsigned char) * key_len);
+	key[0] = 0xBA; key[1] = 0x1F; key[2] = 0x91; key[3] = 0xB2; key[4] = 0x53; key[5] = 0xCD; key[6] = 0x3E;
+	for (i = 0; i < byte_len ; i++) {
+		dec_buf[i] = byte_buf[i] ^ key[i % key_len];
+		dec_len++;
+	}
+	print_byte_sequence_char(dec_buf, dec_len);
 	free(key);
 	key = NULL;
 	return 0;
 }
 
-double square_sum_distribution(unsigned char ctxt[], size_t ctxt_len, size_t key_len)
+double square_sum_distribution(unsigned char byt_arr[], size_t byt_arr_len, size_t key_len)
 {
-	unsigned int ascii_freq[MAX_ASCII + 1];
+	unsigned int char_freq[UCHAR_MAX + 1];
 	unsigned int cand_txt_len;
 	double sqr_sum;
 	unsigned char ch;
-	unsigned int offset;
+	size_t offset;
 	int i;
 
 	/* init */
-	memset(ascii_freq, 0, sizeof(ascii_freq));
-	cand_txt_len = ctxt_len / key_len; //get the total length of candidate letters of ciphered text chosen by the key_len
+	memset(char_freq, 0, sizeof(char_freq));
+	cand_txt_len = byt_arr_len / key_len; //get the total length of candidate letters of byte sequence chosen by the key_len
 	sqr_sum = 0.0;
-	offset = 3; //calculate only for <1st letter + key_len> mod(key_len)
+	offset = 0; //calculate only for <1st letter + key_len> mod(key_len) -> can try for other offsets as well
 
 	/* count each alphabets that are encrpyted with the same key value */
-	while (offset <= ctxt_len) {
-		ch = ctxt[offset]; //get ciphered char from ciphered text
-		ascii_freq[ch]++; //increment the frequency of deciphered char
-		offset += key_len; //skip by key_len in ciphered text
+	while (offset <= byt_arr_len) {
+		ch = byt_arr[offset]; //get ciphered char from byte array
+		char_freq[ch]++; //increment the frequency of deciphered char
+		offset += key_len; //skip by key_len in byte array
 	}
 
 	/* calculate the square sum distribution */
-	for (i = 0; i < MAX_ASCII; i++)
-		sqr_sum += (double) (ascii_freq[i] * ascii_freq[i]) / (double) (cand_txt_len * cand_txt_len);
+	for (i = 0; i <= UCHAR_MAX; i++)
+		sqr_sum += (double) (char_freq[i] * char_freq[i]) / (double) (cand_txt_len * cand_txt_len);
 
 	return sqr_sum;
 }
 
-void guess(unsigned char ctxt[], size_t ctxt_len, size_t key_len, int offset)
+void guess(unsigned char byt_arr[], size_t byt_arr_len, size_t key_len, size_t offset)
 {
 	unsigned char enc_buf[MAX_BUF_LEN];
-	unsigned char key_cand[MAX_BUF_LEN];
-	int offst;
+	size_t offst;
 	size_t len;
-	unsigned char c;
 	int i, j;
 
 	/* init */
 	memset(enc_buf, 0, sizeof(enc_buf));
-	memset(key_cand, 0, sizeof(key_cand));
 	offst = offset;
 
 	/* collect ciphered text that are encrpyted with the same key value based on a specific offset */
 	len = 0;
-	while (offst <= ctxt_len) {
-		enc_buf[len] = ctxt[offst]; //get ciphered char from ciphered text and put it in buffer
+	while (offst <= byt_arr_len) {
+		enc_buf[len] = byt_arr[offst]; //get ciphered char from ciphered text and put it in buffer
 		offst += key_len; //skip by key_len in ciphered text
 		len++;
 	}
 
-	/* brute force key with all ascii code */
-	j = 0;
-	/** go through all ASCII (key can be any char in ASCII) **/
-	for (c = MIN_ASCII; c <= MAX_ASCII; c++) {		
-		int lwr_case, upr_case, punc, spc, nl;
+	/* brute force key with all char */
+	/** go through all char (key can be any char) **/
+	for (i = 0; i <= UCHAR_MAX; i++) {		
+		int lwr_case, upr_case, punc, spc, nl, oth;
 
-		lwr_case = upr_case = punc = spc = nl = 0;
-		/* go through all collected ciphered text */
-		for (i = 0; i < len; i++) {
+		lwr_case = upr_case = punc = spc = nl = oth = 0;
+		/* go through all collected ciphered bytes */
+		for (j = 0; j < len; j++) {
 			unsigned char dec_char; //temp space to store decrypted char
 
 			/* decrypt collected ciphered text with selected char */
 			if (enc_buf[j] != '\n') //new line is not encrypted
-				dec_char = enc_buf[j] ^ c; //decrypt key
+				dec_char = enc_buf[j] ^ (unsigned char) i; //decrypt key
 			else
 				dec_char = enc_buf[j]; //just put it in
 
-			/* check if the decrypted char meets the condition range of ASCII code in assignment instruction */
+			/* check if the decrypted char meets the condition in assignment instructions */
 			if (dec_char >= 'a' && dec_char <= 'z')
 				lwr_case++;
 			else if (dec_char >= 'A' && dec_char <= 'Z')
@@ -154,39 +175,25 @@ void guess(unsigned char ctxt[], size_t ctxt_len, size_t key_len, int offset)
 			else if (dec_char == '\n')
 				nl++;
 			else //everything other than the requirement
-				break;
+				oth++;
 		}
-		/* filter out the candidate keys */
-		if (i == len) {
-			if (lwr_case != 0 && spc != 0) { //1st filter
-				key_cand[j] = c;
-				j++;
-			}
-		}
+
+		/* print the key candidates (still don't get why offset 0 and 1 include oth chars) */
+		//printf("when key is 0x%x: lwr_case: %d, upr_case: %d, spc: %d, punc: %d, nl: %d oth: %d\n", (unsigned char) i, lwr_case, upr_case, spc, punc, nl, oth); 
 	}
-	printf("key candidates of ciphertext with the starting offset of %d are\n", offset);
-	print_byte_sequence(key_cand, j);
 }
 
-void check_space(unsigned char ctxt[], size_t ctxt_len, size_t key_len)
+void print_byte_sequence_hex(unsigned char arr[], size_t len)
 {
-	unsigned char arr1[key_len];
-	unsigned char arr2[key_len];
-	int i, j;
+	int i;
 
-	for (i = 0; i < key_len; i++)
-		arr1[i] = ctxt[i];
+	for (i = 0; i < len; i++)
+		printf("0x%X", arr[i]);
 
-	for (; i < 2 * key_len; i++) {
-		j = i - key_len;
-		arr2[j] = (ctxt[i] ^ arr1[j]) & ' ';
-
-		if (arr2[i - key_len] == 0x00)
-			printf("found space\n");	
-	}
+	printf("\n");
 }
 
-void print_byte_sequence(unsigned char arr[], size_t len)
+void print_byte_sequence_char(unsigned char arr[], size_t len)
 {
 	int i;
 
@@ -194,4 +201,13 @@ void print_byte_sequence(unsigned char arr[], size_t len)
 		printf("%c", arr[i]);
 
 	printf("\n");
+}
+
+unsigned char convert_hexstr_to_byte(char *hex_str)
+{
+	unsigned int val;
+	int i;
+
+	sscanf(hex_str, "%x" , &val);
+	return (unsigned char) val;
 }
